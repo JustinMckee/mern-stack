4c137c9b55fbb3902a8174b6af9dde9e9fac8197
Justin McKee <thisisjustinmckee@gmail.com>
2022-10-13 16:40:15 -0400

    Adds new directory structure

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const api = require('./routes/api/books')
+
 
 const app = express();
 
 connectDB();
+
+app.use('/', api);
 
 app.get('/', (req,res) => res.send('Hello World'));
 
